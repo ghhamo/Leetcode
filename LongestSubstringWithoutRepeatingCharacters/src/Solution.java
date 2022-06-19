9404f7e7573981e1171363ba036e84f5e4c7c53f
ghhamo <93213332+ghhamo@users.noreply.github.com>
2022-06-19 19:20:26 +0400

    Longest substring without repeating characters

--- LongestSubstringWithoutRepeatingCharacters/src/Solution.java
+++ LongestSubstringWithoutRepeatingCharacters/src/Solution.java
@@ -1,5 +1,27 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
+        Map<Character, Integer> visitedChars = new LinkedHashMap<>(s.length());
+        int max = 0;
+        int currentMax = 0;
+        int currentStart = 0;
+        for (int i = 0; i < s.length(); i++) {
+            currentMax++;
+            if (max > s.length() - i + currentMax) {
+                break;
+            }
+            Integer duplicateIndex = visitedChars.get(s.charAt(i));
+            if (duplicateIndex != null && duplicateIndex >= currentStart) {
+                currentStart = duplicateIndex + 1;
+                currentMax = i - duplicateIndex;
+            }
+            if (max < currentMax) {
+                max = currentMax;
+            }
+            visitedChars.put(s.charAt(i), i);
+        }
+        return max;
     }
 }
