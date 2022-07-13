817a434a6c35d3a6ed8336ffb2c275e53d052d15
ghhamo <93213332+ghhamo@users.noreply.github.com>
2022-07-13 18:57:26 +0400

    delete main

--- LongestSubstringWithoutRepeatingCharacters/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.lengthOfLongestSubstring("abbaacw"));
-    }
-}
