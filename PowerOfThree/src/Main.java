9ec95ee3137dad6918909fec9f63e2316e725232
ghhamo <93213332+ghhamo@users.noreply.github.com>
2023-05-26 19:11:16 +0400

    Update Main.java

--- PowerOfThree/src/Main.java
+++ PowerOfThree/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.isPowerOfTwoByIterative(27));
-        System.out.println(solution.isPowerOfTwoByRecursive(29));
+        System.out.println(solution.isPowerOfThreeByIterative(27));
+        System.out.println(solution.isPowerOfThreeByRecursive(29));
     }
 }
