d6b64f7e0274645b2ad5c07ae821f137a1253e92
ghhamo <93213332+ghhamo@users.noreply.github.com>
2024-03-22 19:18:15 +0400

    add and commit fib and merge main methods

---
+++ FibonacciNumber/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.print(solution.fibByIterative(13) + ", ");
+        System.out.print(solution.fibByRecursion(14) + ", ");
+    }
+}
---
+++ MergeSortedArray/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.merge(new int[]{4, 5, 6, 0, 0, 0}, 3, new int[]{1, 2, 3}, 3);
+    }
+}
