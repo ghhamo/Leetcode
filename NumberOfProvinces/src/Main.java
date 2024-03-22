6576ed38bf2a3182d3005cf49d58d202af7d3625
ghhamo <93213332+ghhamo@users.noreply.github.com>
2024-03-22 18:53:24 +0400

    last commit

---
+++ NumberOfProvinces/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.findCircleNum(new int[][]{new int[]{1,0,0,1},new int[]{0,1,1,0},new int[]{0,1,1,1}, new int[]{1,0,1,1}});
+    }
+}
+
+/*
+new int[][]{new int[]{1,1,0},new int[]{1,1,0},new int[]{0,0,1}}
+new int[][]{new int[]{1,0,0},new int[]{0,1,0},new int[]{0,0,1}}
+new int[][]{new int[]{1,0,0,1},new int[]{0,1,1,0},new int[]{0,1,1,1}, new int[]{1,0,1,1}}
+*/
